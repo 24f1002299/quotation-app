2b22f1d0248a17c04e7bb0684882d7f04361c6b3
hasratmd697 <hasratmd697@gmail.com>
2026-09-19 23:26:53 +0530

    Refactor code structure for improved readability and maintainability

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.contractorpoc.contractor_quote_poc"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "30.0.16248370"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
